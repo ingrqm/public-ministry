72f3810560aad4c67144d6eb2911aa096c7bd2d4
ingrqm <contact@imaxart.com>
2023-11-06 10:35:58 +0100

    chore: update packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = withNextIntl(nextConfig);
