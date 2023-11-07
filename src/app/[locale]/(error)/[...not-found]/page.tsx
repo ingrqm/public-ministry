7cadfcd25c796e084367fac6b30dd34ea391d69b
Ingram Kalina <51913097+ingrqm@users.noreply.github.com>
2023-11-07 21:09:14 +0100

    feat: error flow page not found (#5)

--- src/app/[locale]/(error)/[...not-found]/page.tsx
+++ src/app/[locale]/(error)/[...not-found]/page.tsx
@@ -1,6 +1,13 @@
+import { useTranslations } from 'next-intl';
 import { getTranslator } from 'next-intl/server';
+import Link from 'next/link';
 
 import type { Locale } from '@/i18n';
+
+import { Button } from '@/components/ui/button';
+import { Typography } from '@/components/ui/typography';
+
+import NotFoundImage from '@/assets/images/not-found';
 
 type Props = {
   params: {
@@ -17,5 +24,20 @@ export async function generateMetadata({ params: { locale } }: Props) {
 }
 
 export default function NotFound() {
-  return <div>not found</div>;
+  const t = useTranslations('page.error.not-found');
+
+  return (
+    <div className="flex flex-col items-center">
+      <NotFoundImage />
+      <Typography.Title level={4} className="mt-9">
+        {t('body.heading')}
+      </Typography.Title>
+      <Typography.Paragraph className="mt-2">{t('body.paragraph')}</Typography.Paragraph>
+      <Link href={'/dashboard'}>
+        <Button size="lg" className="mt-6">
+          {t('body.link')}
+        </Button>
+      </Link>
+    </div>
+  );
 }
