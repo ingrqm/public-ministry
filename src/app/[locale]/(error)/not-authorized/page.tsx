a632a026051289f84f413876ec41b973b4f7e696
Ingram Kalina <51913097+ingrqm@users.noreply.github.com>
2023-11-07 21:35:49 +0100

    feat: PM-33 error flow page not authorized (#6)
    * feat: error flow page not authorized
    
    * fix: intl issue
    
    * chore: comment fix solution

--- src/app/[locale]/(error)/not-authorized/page.tsx
+++ src/app/[locale]/(error)/not-authorized/page.tsx
@@ -1,6 +1,16 @@
+import { useTranslations } from 'next-intl';
 import { getTranslator } from 'next-intl/server';
+import Link from 'next/link';
 
 import type { Locale } from '@/i18n';
+
+import { Button } from '@/components/ui/button';
+import { Typography } from '@/components/ui/typography';
+
+import NotAuthorizedImage from '@/assets/images/not-authorized';
+
+// This resolve intl issue on build
+export const dynamic = 'force-dynamic';
 
 type Props = {
   params: {
@@ -17,5 +27,20 @@ export async function generateMetadata({ params: { locale } }: Props) {
 }
 
 export default function NotAuthorized() {
-  return <div>not authorized</div>;
+  const t = useTranslations('page.error.not-authorized');
+
+  return (
+    <div className="flex flex-col items-center">
+      <NotAuthorizedImage />
+      <Typography.Title level={4} className="mt-9">
+        {t('body.heading')}
+      </Typography.Title>
+      <Typography.Paragraph className="mt-2">{t('body.paragraph')}</Typography.Paragraph>
+      <Link href={'/dashboard'}>
+        <Button size="lg" className="mt-6">
+          {t('body.link')}
+        </Button>
+      </Link>
+    </div>
+  );
 }
