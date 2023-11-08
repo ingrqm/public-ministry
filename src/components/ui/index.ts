8e26e06fa65f12c08b0ee76a562a5db74f863ea3
Ingram Kalina <51913097+ingrqm@users.noreply.github.com>
2023-11-08 13:45:45 +0100

    feat: PM-16 component label (#16)
    * feat: component label
    
    * refactor: import type

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,6 @@ export * from './button';
 export * from './checkbox';
 export * from './dropdown-menu';
 export * from './input';
+export * from './label';
 export * from './select';
 export * from './typography';
