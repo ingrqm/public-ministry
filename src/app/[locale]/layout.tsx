2e99017ec0d2c173b9e6daa85b24ab0740386967
Ingram Kalina <51913097+ingrqm@users.noreply.github.com>
2023-11-08 23:40:20 +0100

    feat: favicon (#24)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -36,6 +36,21 @@ export default async function RootLayout({ children, params: { locale } }: RootL
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon-194x194.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/site.webmanifest" />
+        <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#09090b" />
+        <link rel="shortcut icon" href="/assets/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#09090b" />
+        <meta name="msapplication-TileImage" content="/assets/mstile-144x144.png" />
+        <meta name="msapplication-config" content="/assets/browserconfig.xml" />
+        <meta name="theme-color" content="#09090b" />
+      </head>
+
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
