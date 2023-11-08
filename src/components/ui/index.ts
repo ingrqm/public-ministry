84d772417df3bcfcd505d7cf10e5566550678c65
ingrqm <contact@imaxart.com>
2023-11-08 15:19:15 +0100

    feat: export form

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,7 @@ export * from './button';
 export * from './card';
 export * from './checkbox';
 export * from './dropdown-menu';
+export * from './form';
 export * from './input';
 export * from './label';
 export * from './select';
