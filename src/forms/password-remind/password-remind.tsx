08a195129753b7493fb3e162e4f1892958a3e5a6
ingrqm <contact@imaxart.com>
2023-11-12 00:22:17 +0100

    refactor: react import

--- src/forms/password-remind/password-remind.tsx
+++ src/forms/password-remind/password-remind.tsx
@@ -2,7 +2,9 @@
 
 import { useTranslations } from 'next-intl';
 
-import { useForm } from '@/hooks';
+import { api } from '@/api';
+
+import { useForm, useLocale } from '@/hooks';
 
 import { Button, Input } from '@/components/ui';
 
@@ -10,15 +12,25 @@ import { defaultValues, FormInputs, type FormTypes, schema } from './password-re
 
 export const PasswordRemindForm = () => {
   const t = useTranslations('form');
+  const locale = useLocale();
 
-  const [Form, FormField] = useForm<FormTypes>({
+  const [
+    Form,
+    FormField,
+    {
+      formState: { isSubmitting },
+    },
+  ] = useForm<FormTypes>({
     schema,
     defaultValues,
+    key: 'password-remind',
   });
 
-  const handleSubmit = (values: FormTypes) => {
-    console.log(values);
-  };
+  const handleSubmit = (values: FormTypes) =>
+    api.passwordReset.request({
+      locale,
+      email: values.email,
+    });
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -26,7 +38,7 @@ export const PasswordRemindForm = () => {
         <Input type="text" placeholder={t('input.email.placeholder')} />
       </FormField>
 
-      <Button className="mt-2 w-full" type="submit">
+      <Button className="mt-2 w-full" type="submit" disabled={isSubmitting} isLoading={isSubmitting}>
         {t('password-remind.button.submit.label')}
       </Button>
     </Form>
