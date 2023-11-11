10a3bd4e58365aa0ddbb3b006cb40ed65dc3430a
ingrqm <contact@imaxart.com>
2023-11-11 07:43:10 +0100

    feat: allow server actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
