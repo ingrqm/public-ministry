381fcb5cb0f32f98beef9332d67a209289f5a3cb
ingrqm <contact@imaxart.com>
2023-11-12 01:52:08 +0100

    feat: component toast

--- src/forms/password-remind/password-remind.tsx
+++ src/forms/password-remind/password-remind.tsx
@@ -14,6 +14,7 @@ export const PasswordRemindForm = () => {
   const [Form, FormField] = useForm<FormTypes>({
     schema,
     defaultValues,
+    key: 'password-remind',
   });
 
   const handleSubmit = (values: FormTypes) => {
